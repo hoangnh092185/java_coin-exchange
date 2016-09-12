3d69885cb8cdb3c92053c109f219837fdcb5c5d3
Nhat Hoang and Stephen Emery <stephen@>
2016-09-12 13:11:36 -0700

    Add final routes and coins left output.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,33 +8,43 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    ChangeMachine machine = new ChangeMachine();
 
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/hello.vtl" );
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/favorite_photos", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/favorite_photos.vtl" );
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/form", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/form.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/greeting_card", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   String recipient = request.queryParams("recipient");
-    //   String sender = request.queryParams("sender");
-    //   model.put("recipient", recipient);
-    //   model.put("sender", sender);
-    //   model.put("template", "templates/greeting_card.vtl");
-    //   return new ModelAndView(model, "templates/layout.vtl");
-    // }, new VelocityTemplateEngine());
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String stringChange  = request.queryParams("change");
+      Float floatChange = Float.parseFloat(stringChange);
+      String output;
+      if (floatChange >8.20){
+        output = String.format("Error: Not enough coins.");
+      } else {
+        output = machine.makeChange(floatChange);
+      }
+
+      // Further Exploration: Loop to create all strings and routes.
+      // String[] coins = {"Quarter", "Dimes", "Nickels", "Pennies"};
+      // for (String coin : coins) {
+      //   String (coin)Left = String.format("There are " + machine.get(coin)() + " (coin) left.");
+      //   model.put((coin)+"Left", (coin)"Left");
+      // }
+
+      String quartersLeft = String.format("There are " + machine.getQuarters() + " quarters left.<br>");
+      String dimesLeft = String.format("There are "+ machine.getDimes() + " dimes left.<br>");
+      String nickelsLeft = String.format("There are "+ machine.getNickels() + " nickels left.<br>");
+      String penniesLeft = String.format("There are "+ machine.getPennies() + " pennies left.");
+      model.put("quartersLeft", quartersLeft);
+      model.put("dimesLeft", dimesLeft);
+      model.put("nickelsLeft", nickelsLeft);
+      model.put("penniesLeft", penniesLeft);
+      model.put("change", output);
+      model.put("template", "templates/results.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
